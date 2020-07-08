f2b772d5c218b25aa5e00b10d5e3c94559b9ec1c
Daniel Wei <dominum15@outlook.com>
2020-07-07 22:11:18 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.335cb376a401a49da3d114a2ceabcd86.js"
+  "/precache-manifest.a453867746ac21cebf22d320f8776162.js"
 );
 
 self.addEventListener('message', (event) => {
